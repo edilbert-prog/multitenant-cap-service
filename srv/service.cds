4e9fe3c6cefdeb61034fe1101e8222b69e93ff7d
EdilbertJ <Edilbert.Jana@sirobilt.com>
2026-03-10 13:08:59 +0530

    demonstration of multi tenant create order and view the orders

--- srv/service.cds
+++ srv/service.cds
@@ -1,8 +1,7 @@
 using { Orders as DBOrders, Tenants as DBTenants } from '../db/schema';
 
 service OrdersService {
-
-  entity Orders  as projection on DBOrders;
+  entity Orders  as projection on DBOrders; 
   entity Tenants as projection on DBTenants;
 
   function Welcome() returns String;
--- srv/service.js
+++ srv/service.js
@@ -4,31 +4,19 @@ module.exports = cds.service.impl(function () {
 
   const { Orders } = this.entities
 
-
-  // Custom endpoint to return a welcome message for the current tenant
+  // Welcome message
   this.on('Welcome', (req) => {
-    const tenant = req.tenant
-    return `Welcome ${tenant} 🚀`
+    return `Welcome ${req.tenant} 🚀`
   })
 
-
-  // Before inserting a new order, automatically attach the tenant
+  // Attach tenant automatically when creating
   this.before('CREATE', Orders, (req) => {
     req.data.tenant = req.tenant
   })
 
-
-  // Before reading orders, filter results so each tenant only sees its own data
+  // Restrict reads to current tenant
   this.before('READ', Orders, (req) => {
-
-    const tenant = req.tenant
-
-    if (!req.query.SELECT.where) {
-      req.query.SELECT.where = [{ ref: ['tenant'] }, '=', { val: tenant }]
-    } else {
-      req.query.SELECT.where.push('and', { ref: ['tenant'] }, '=', { val: tenant })
-    }
-
+    req.query.where({ tenant: req.tenant })
   })
 
 })
